8e41c622044058f9fbfcfbbff3eff6ffc278babb
cmgpgerrracarlos <cmgpguerracarlos@gmail.com>
2021-01-21 15:02:26 -0300

    add connection to mysql create database amd tables

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-const express = require('express')
 const mysql = require('mysql')
 const con = mysql.createConnection({
     host:'localhost',
@@ -7,25 +6,15 @@ const con = mysql.createConnection({
     database:'trivia'
 });
 
+const data = [];
 
+con.connect();
 
-const data = [
-    {id:1, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:2, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:3, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:4, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:5, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:6, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:7, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:8, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:9, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:10, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:11, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:12, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:13, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:14, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"},
-    {id:15, title:"el titulo de la app",op1:"uno",op2:"dos",op3:"tres",op4:"cuatro",op5:"cinco",correcto:"tres"} 
-];
+con.query('SELECT * FROM data',(error,result)=>{
+    if(error) throw error;
+    data.push(result);
+});
 
+con.end();
  
 module.exports = data
