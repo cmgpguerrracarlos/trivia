de67ed0995c437dc73d94c7c10e26cee87d5d194
cmgpgerrracarlos <cmgpguerracarlos@gmail.com>
2021-01-22 08:19:13 -0300

    add onclickhandler

--- src/Card.js
+++ src/Card.js
@@ -21,12 +21,18 @@ class Card extends Component {
         this.setState({
             question:"La pregunta se va a colocar aca, hay una unica respuesta posible",
             op1:"Opcion 1",op2:"Opcion 2",op3:"Opcion 3",op4:"Opcion 4",op5:"Opcion 5",
-            correcta:"Opcion 1",color:"yellow"
+            correcta:"3",color:"yellow"
         });
     }
 
     onClickHandler = (e)=>{
-        console.log("tocado el  boton");
+        let c = e.currentTarget.name;
+        if(c === this.state.correcta){
+            console.log("Correcto");
+        }else{
+            console.log("No correcto");
+        }
+        
     }
     render() {
         return (
@@ -35,11 +41,11 @@ class Card extends Component {
                     <p>{this.state.question}</p>
                 </div>
                 <div class="main-container">
-                    <button className={this.state.color} onClick={this.onClickHandler} name="o1">{this.state.op1}</button>
-                    <button onClick={this.onClickHandler} name="o2">{this.state.op2}</button>
-                    <button onClick={this.onClickHandler} name="o3">{this.state.op3}</button>
-                    <button onClick={this.onClickHandler} name="o4">{this.state.op4}</button>
-                    <button onClick={this.onClickHandler} name="o5">{this.state.op5}</button>
+                    <button onClick={this.onClickHandler} name="1">{this.state.op1}</button>
+                    <button onClick={this.onClickHandler} name="2">{this.state.op2}</button>
+                    <button onClick={this.onClickHandler} name="3">{this.state.op3}</button>
+                    <button onClick={this.onClickHandler} name="4">{this.state.op4}</button>
+                    <button onClick={this.onClickHandler} name="5">{this.state.op5}</button>
                 </div>
              </header>
         )
