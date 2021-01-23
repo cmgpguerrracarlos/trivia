13a4d33d67371ec909d8de66a0b5c84911de94c3
cmgplaptopblack <cmgpguerracarlos@gmail.com>
2021-01-22 22:17:00 -0300

    add color right and wrong

--- src/Card.js
+++ src/Card.js
@@ -22,7 +22,7 @@ class Card extends Component {
         this.setState({
             question:"La pregunta se va a colocar aca, hay una unica respuesta posible",
             op1:"Opcion 1",op2:"Opcion 2",op3:"Opcion 3",op4:"Opcion 4",op5:"Opcion 5",
-            correcta:"1",color:"yellow"
+            correcta:"1",color:"yellow",press:false
         });
     }
 
@@ -45,11 +45,11 @@ class Card extends Component {
                     <p>{this.state.question}</p>
                 </div>
                 <div class="main-container background">
-                    <button onClick={this.onClickHandler} name="1" className={(this.state.press)?"background":this.state.correcta === "1"?"green":"red"} >{this.state.op1} </button>
-                    <button onClick={this.onClickHandler} name="2" className={this.state.correcta === "2"?"green":"red"}>{this.state.op2}</button>
-                    <button onClick={this.onClickHandler} name="3" className={this.state.correcta === "3"?"green":"red"}>{this.state.op3}</button>
-                    <button onClick={this.onClickHandler} name="4" className={this.state.correcta === "4"?"green":"red"}>{this.state.op4}</button>
-                    <button onClick={this.onClickHandler} name="5"className={this.state.correcta === "5"?"green":"red"}>{this.state.op5}</button>
+                    <button onClick={this.onClickHandler} name="1" className={(!this.state.press)?"background":this.state.correcta === "1"?"green":"red"} >{this.state.op1} </button>
+                    <button onClick={this.onClickHandler} name="2" className={(!this.state.press)?"background":this.state.correcta === "2"?"green":"red"}>{this.state.op2}</button>
+                    <button onClick={this.onClickHandler} name="3" className={(!this.state.press)?"background":this.state.correcta === "3"?"green":"red"}>{this.state.op3}</button>
+                    <button onClick={this.onClickHandler} name="4" className={(!this.state.press)?"background":this.state.correcta === "4"?"green":"red"}>{this.state.op4}</button>
+                    <button onClick={this.onClickHandler} name="5"className={(!this.state.press)?"background":this.state.correcta === "5"?"green":"red"}>{this.state.op5}</button>
                 </div>
              </header>
         )
