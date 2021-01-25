1c91837d887cbf27a88b2db607a2de969aa1ea18
cmgpgerrracarlos <cmgpguerracarlos@gmail.com>
2021-01-25 04:14:30 -0300

    fixed gave the power to the controller ook the function from card

--- src/Card.js
+++ src/Card.js
@@ -1,67 +1,21 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class Card extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = [{
-            question:"",
-            op1:"",
-            op2:"",
-            op3:"",
-            op4:"",
-            op5:"",
-            correcta:"",
-            press:false
-        }]
-    }
-    
-    componentDidMount(){
-        this.setState({
-            question:this.props.data.q,
-            op1:this.props.data.op1,
-            op2:this.props.data.op2,
-            op3:this.props.data.op3,
-            op4:this.props.data.op4,
-            op5:this.props.data.op5,
-            correcta:this.props.data.correcta,
-            press:this.props.data.press
-        });
-    }
-
-    onClickHandler = (e)=>{
-        let c = e.currentTarget.name;
-        if(c === this.state.correcta){
-            console.log("Correcto");
-        }else{
-            console.log("No correcto");
-        }
-        this.setState({
-            press:true
-        });
-        
-    }
-
-    onClickNext = (e)=>{
-        this.setState({
-            press:false
-        })
-    }
+class Card extends Component {    
     render() {
         return (
             <header className="App-header">
                 <div className="head-container">
-                    <p>{this.state.question}</p>
+                    <p>{this.props.data.question}</p>
                 </div>
                 <div class="main-container background">
-                    <button onClick={this.onClickHandler} name="1" className={(!this.state.press)?"background":this.state.correcta === "1"?"green":"red"} >{this.state.op1} </button>
-                    <button onClick={this.onClickHandler} name="2" className={(!this.state.press)?"background":this.state.correcta === "2"?"green":"red"}>{this.state.op2}</button>
-                    <button onClick={this.onClickHandler} name="3" className={(!this.state.press)?"background":this.state.correcta === "3"?"green":"red"}>{this.state.op3}</button>
-                    <button onClick={this.onClickHandler} name="4" className={(!this.state.press)?"background":this.state.correcta === "4"?"green":"red"}>{this.state.op4}</button>
-                    <button onClick={this.onClickHandler} name="5"className={(!this.state.press)?"background":this.state.correcta === "5"?"green":"red"}>{this.state.op5}</button>
+                    <button onClick={this.props.onClickHandler} name="1" className={(!this.props.data.press)?"background":this.props.data.correcta === "1"?"green":"red"} >{this.props.data.op1} </button>
+                    <button onClick={this.props.onClickHandler} name="2" className={(!this.props.data.press)?"background":this.props.data.correcta === "2"?"green":"red"}>{this.props.data.op2}</button>
+                    <button onClick={this.props.onClickHandler} name="3" className={(!this.props.data.press)?"background":this.props.data.correcta === "3"?"green":"red"}>{this.props.data.op3}</button>
+                    <button onClick={this.props.onClickHandler} name="4" className={(!this.props.data.press)?"background":this.props.data.correcta === "4"?"green":"red"}>{this.props.data.op4}</button>
+                    <button onClick={this.props.onClickHandler} name="5"className={(!this.props.data.press)?"background":this.props.data.correcta === "5"?"green":"red"}>{this.props.data.op5}</button>
                 </div>
-                <button onClick={this.onClickNext}>next</button>
+                <button onClick={this.props.onClickNext}>next</button>
              </header>
         )
     }
--- src/Controller.js
+++ src/Controller.js
@@ -4,25 +4,50 @@ import Card from "./Card";
 export default class Controller extends Component {
     constructor(props){
         super(props)
-        this.state=[{
-            question:"La pregunta se va a colocar aca, hay una unica respuesta posible",
-                op1:"Opcion 1",op2:"Opcion 2",op3:"Opcion 3",op4:"Opcion 4",op5:"Opcion 5",
-                correcta:"1",color:"yellow",press:false
-        }]
+        this.state={
+            data:{}
+        }
     }
 
     componentDidMount(){
-        this.setState({
+        this.setState({data:{
             question:"La pregunta se va a colocar aca, hay una unica respuesta posible",
                 op1:"Opcion 1",op2:"Opcion 2",op3:"Opcion 3",op4:"Opcion 4",op5:"Opcion 5",
-                correcta:"1",color:"yellow",press:false
+                correcta:"1",press:false
+        }});
+    }
+
+    onClickHandler = (e)=>{
+        let c = e.currentTarget.name;
+        if(c === this.state.correcta){
+            console.log("Correcto");
+        }else{
+            console.log("No correcto");
+        }
+        this.setState({
+            data:{
+                question:"La pregunta se va a colocar aca, hay una unica respuesta posible",
+                op1:"Opcion 1",op2:"Opcion 2",op3:"Opcion 3",op4:"Opcion 4",op5:"Opcion 5",
+                correcta:"1",press:true
+            }
         });
+        
+    }
+
+    onClickNext = (e)=>{
+        this.setState({
+            data:{
+                question:"La pregunta se va a colocar aca, hay una unica respuesta posible",
+                op1:"Opcion 1",op2:"Opcion 2",op3:"Opcion 3",op4:"Opcion 4",op5:"Opcion 5",
+                correcta:"1",press:false
+            }
+        })
     }
 
     render() {
         return (
             <div className="App">
-            <Card q={this.state.question}/>
+            <Card data={this.state.data} onClickHandler={this.onClickHandler} onClickNext={this.onClickNext}/>
           </div>
         )
     }
