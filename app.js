e501dc15346f1c33d084737253551017729e87dd
cmgpgerrracarlos <cmgpguerracarlos@gmail.com>
2021-02-09 14:39:15 -0300

    change text of app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use('/api',routeApi);
 
 app.use('/',(req,res)=>{
-    res.send("<h1>Bienvenido a la trivia, para ir a la api  <a href='/api'>hace click aca</a></h1>");
+    res.send("<h1>Bienvenido a la trivia <br/>Api:  <a href='/api'>hace click aca</a></h1>");
 });
 
 app.listen(9000,()=>{
